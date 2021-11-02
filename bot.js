904a3c0726349ee1368ae6770f3c7bbe4c7dfd91
Lufrut <56224664+Lufrut@users.noreply.github.com>
2021-11-02 22:27:39 +0200

    try to fix error with it in chat

--- bot.js
+++ bot.js
@@ -98,7 +98,7 @@ bot.onText(new RegExp('/top10world_slaves'),async (msg)=>{
         console.log(e)
     }
 })
-bot.onText(new RegExp('/steal_slaves (@.*)'),async (msg,match)=>{
+bot.onText(new RegExp('/steal_slaves (@.*)'),async (msg,[source,match])=>{
     try{
         const {chat: {id}} = msg
         match = match.substr(match.indexOf("@") + 1);

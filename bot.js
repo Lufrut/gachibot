104b8dcbac451cdeb4d5c60fc2ef6d30bf6641f0
Lufrut <superkolasar5@gmail.com>
2022-03-02 15:53:49 +0100

    add logs

--- bot.js
+++ bot.js
@@ -1,4 +1,5 @@
 require('dotenv').config()
+const winston = require('winston');
 const TelegramBot = require('node-telegram-bot-api')
 const sequelize = require('./db')
 const models = require('./models/general')
@@ -16,6 +17,28 @@ const start = async () => {
     }
 }
 start()
+const logger = winston.createLogger({
+    level: 'info',
+    format: winston.format.json(),
+    defaultMeta: { service: 'user-service' },
+    transports: [
+        //
+        // - Write all logs with importance level of `error` or less to `error.log`
+        // - Write all logs with importance level of `info` or less to `combined.log`
+        //
+        new winston.transports.File({ filename: 'error.log', level: 'error' }),
+        new winston.transports.File({ filename: 'combined.log' }),
+    ],
+});
+//
+// If we're not in production then log to the `console` with the format:
+// `${info.level}: ${info.message} JSON.stringify({ ...rest }) `
+//
+if (process.env.NODE_ENV !== 'production') {
+    logger.add(new winston.transports.Console({
+        format: winston.format.simple(),
+    }));
+}
 bot.on('new_chat_members',async (msg) =>{
     const {chat: {id}} = msg
     await bot.sendAnimation(id,buffer)

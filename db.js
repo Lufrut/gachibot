987947a01ad82ff451186ce13b003a3f465f7a5c
Lufrut <superkolasar5@gmail.com>
2022-03-04 13:49:58 +0100

    fix deadlocks

--- db.js
+++ db.js
@@ -5,7 +5,13 @@ module.exports = new Sequelize(
     process.env.DB_PASSWORD,
     {
         dialect: 'postgres',
+        retry: {
+            match: [/Deadlock/i],
+            max: 3, // Maximum retry 3 times
+            backoffBase: 1000, // Initial backoff duration in ms. Default: 100,
+            backoffExponent: 1.5, // Exponent to increase backoff each try. Default: 1.1
+        },
         host: process.env.DB_HOST,
-        port: process.env.DB_PORT
+        port: process.env.DB_PORT,
     }
 );
